67afa7057c2ea242ab9b187d0e928e1d1b294d35
Peshooo <petar.nyagolov.2000@gmail.com>
2020-10-18 13:19:11 +0300

    migrate to postgresql

--- RecordStore/src/main/resources/db/migration/V002__standard_records_table.sql
+++ RecordStore/src/main/resources/db/migration/V002__standard_records_table.sql
@@ -1,8 +1,17 @@
-CREATE TABLE `standard_records` (
-    `game_uuid` char(36) NOT NULL,
-    `nickname` char(36) NOT NULL,
-    `score` bigint NOT NULL,
-    `created_at` datetime NOT NULL,
-    PRIMARY KEY (`game_uuid`),
-    INDEX `score_index` (`score`)
+--CREATE TABLE `standard_records` (
+--    `game_uuid` char(36) NOT NULL,
+--    `nickname` char(36) NOT NULL,
+--    `score` bigint NOT NULL,
+--    `created_at` datetime NOT NULL,
+--    PRIMARY KEY (`game_uuid`),
+--    INDEX `score_index` (`score`)
+--);
+
+CREATE TABLE IF NOT EXISTS standard_records (
+    game_uuid char(36) NOT NULL PRIMARY KEY,
+    nickname char(36) NOT NULL,
+    score bigint NOT NULL,
+    created_at TIMESTAMP NOT NULL
 );
+
+CREATE INDEX IF NOT EXISTS standard_score_index ON standard_records (score);
